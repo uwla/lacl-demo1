8011b0fec1d7c7fe28cd6fa915a3af263ad03398
uwla <uwlalinux@gmail.com>
2023-03-03 17:36:21 -0300

    adapt seeders for demonstration purposes

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -16,21 +16,28 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory(100)->create();
-
-        // create per-model permissions for demonstration purposes
-        $a1 = Article::find(1);
-        $a2 = Article::find(2);
-        $u1 = User::find(1);
-        $u2 = User::find(2);
-        $u3 = User::find(3);
-        $u4 = User::find(4);
-
+        // create per-article permissions for demonstration purposes
+        $a1 = Article::factory()->createOne();
+        $a2 = Article::factory()->createOne();
         $a1->createCrudPermissions();
         $a2->createCrudPermissions();
+
+        // password
+        $pass = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+
+        // these are the users which will have per-article permissions
+        $u1 = User::create(['name' => 'u1', 'email' => 'u1@example.test', 'password' => $pass]);
+        $u2 = User::create(['name' => 'u2', 'email' => 'u2@example.test', 'password' => $pass]);
+        $u3 = User::create(['name' => 'u3', 'email' => 'u3@example.test', 'password' => $pass]);
+        $u4 = User::create(['name' => 'u4', 'email' => 'u4@example.test', 'password' => $pass]);
+
+        // attach the permissions
         $a1->attachCrudPermissions($u1);
         $a2->attachCrudPermissions($u2);
         $a1->attachDeletePermission($u3);
         $a2->attachDeletePermission($u4);
+
+        // generate many articles
+        Article::factory(100)->create();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // generate some random users
         User::factory(30)->create();
         $users = User::all();
         $roles = Role::all();
@@ -26,5 +27,37 @@ class UserSeeder extends Seeder
             $role = $roles->random(1)[0];
             $user->addRole($role);
         }
+
+        // pick the main roles
+        $role_admin         = Role::where(['name' => 'admin'])->first();
+        $role_editor        = Role::where(['name' => 'editor'])->first();
+        $role_user_manager  = Role::where(['name' => 'user manager'])->first();
+        $role_chief_manager = Role::where(['name' => 'chief manager'])->first();
+
+        // password
+        $pass = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+
+        // create one user for each main role
+        // the thing is, the email are easy to remember for these users
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.test',
+            'password' => $pass
+        ])->addRole($role_admin);
+        User::create([
+            'name' => 'editor',
+            'email' => 'editor@example.test',
+            'password' => $pass
+        ])->addRole($role_editor);
+        User::create([
+            'name' => 'User manager',
+            'email' => 'user_manager@example.test',
+            'password' => $pass
+        ])->addRole($role_user_manager);
+        User::create([
+            'name' => 'manager',
+            'email' => 'manager@example.test',
+            'password' => $pass
+        ])->addRole($role_chief_manager);
     }
 }
